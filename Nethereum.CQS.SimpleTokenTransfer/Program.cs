339fe796c273d43d696e3100a2532a69c2c8d209
Juan Blanco <juanfranblanco@hotmail.com>
2017-12-22 11:42:53 +0000

    Adding Balance to the sample

--- Nethereum.CQS.SimpleTokenTransfer/Program.cs
+++ Nethereum.CQS.SimpleTokenTransfer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Nethereum.ABI.FunctionEncoding.Attributes;
@@ -14,8 +15,39 @@ namespace Nethereum.CQS.SimpleTokenTransfer
     {
         static void Main(string[] args)
         {
-            TransferAsync().Wait();
+            BalanceAsync().Wait();
             Console.ReadLine();
+            //TransferAsync().Wait();
+            Console.ReadLine();
+        }
+
+
+        public static async Task BalanceAsync()
+        {
+            var senderAddress = "0x7c31560552170ce96c4a7b018e93cddc19dc61b6";
+            var contractAddress = "0x0d8775f648430679a709e98d2b0cb6250d2887ef";
+
+            //no private key we are not signing anything
+            var web3 = new Web3.Web3("https://mainnet.infura.io");
+
+            var balanceOfFunctionMessage = new BalanceOfFunction()
+            {
+                Owner = senderAddress,
+            };
+
+            var balanceHandler = web3.Eth.GetContractQueryHandler<BalanceOfFunction>();
+            var balance = await balanceHandler.QueryAsync<BigInteger>(balanceOfFunctionMessage, contractAddress);
+            Console.WriteLine("Balance of token: " + Web3.Web3.Convert.FromWei(balance));
+
+    }
+
+        [Function("balanceOf", "uint256")]
+        public class BalanceOfFunction : ContractMessage
+        {
+
+            [Parameter("address", "_owner", 1)]
+            public string Owner { get; set; }
+
         }
 
         public static async Task TransferAsync()
@@ -31,11 +63,12 @@ namespace Nethereum.CQS.SimpleTokenTransfer
             {
                 FromAddress = senderAddress,
                 To = receiverAddress,
-                TokenAmount = 1000
+                TokenAmount = Web3.Web3.Convert.ToWei(1000)
             };
 
             var transferHandler = web3.Eth.GetContractTrasactionHandler<TransferFunction>();
-            var transferReceipt = await transferHandler.SendRequestAndWaitForReceiptAsync(transactionMessage, contractAddress);
+            var transactionHash = await transferHandler.SendRequestAsync(transactionMessage, contractAddress);
+            Console.WriteLine(transactionHash);
 
         }
 
@@ -47,7 +80,7 @@ namespace Nethereum.CQS.SimpleTokenTransfer
             public string To { get; set; }
 
             [Parameter("uint256", "_value", 2)]
-            public int TokenAmount { get; set; }
+            public BigInteger TokenAmount { get; set; }
         }
     }
 }
